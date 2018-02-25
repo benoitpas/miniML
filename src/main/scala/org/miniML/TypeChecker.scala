71b50b2fb917f1edb9aa6381d6d8379f8a970d23
benoitpas <benoitpas@users.noreply.github.com>
2018-02-25 21:00:51 +0000

    Added binary operations to type checker

--- src/main/scala/org/miniML/TypeChecker.scala
+++ src/main/scala/org/miniML/TypeChecker.scala
@@ -1,17 +1,30 @@
 package org.miniML
 
-import org.miniML.parser.Expression
-import org.miniML.parser.Integer
+import org.miniML.parser._
 
 object TypeChecker {
 
+
     def apply(e: Expression): Either[String, EType] = e match {
         case Integer(i) => Right(Nat())
+        case Division(e1, e2) => operation(e1, e2)
+        case Product(e1, e2) => operation(e1, e2)
+        case Sum(e1, e2) => operation(e1, e2)
+        case Minus(e1, e2) => operation(e1, e2)
+    }
+
+    def operation(e1: Expression, e2: Expression): Either[String, EType] = (TypeChecker(e1), TypeChecker(e2)) match {
+        case (Right(Nat()), Right(Nat())) => Right(Nat())
+        case (Right(Nat()), Right(e)) => Left(e +" should be 'integer'.")
+        case (Right(e), Right(_)) => Left(e +" should be 'integer'.")
+        case (Left(s1), Left(s2)) => Left(s1 + s2)
+        case (Left(s1), _) => Left(s1)
+        case (_, Left(s2)) => Left(s2)
     }
 
     trait EType
 
     sealed case class Nat() extends EType
 
-    sealed case class F(from:EType,to:EType) extends EType
+    sealed case class F(from: EType, to: EType) extends EType
 }
--- src/test/scala/org/miniML/TypeCheckerSuite.scala
+++ src/test/scala/org/miniML/TypeCheckerSuite.scala
@@ -1,16 +1,28 @@
 package org.miniML
 
 import org.junit.runner.RunWith
+import org.miniML.TypeChecker.EType
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-
-import org.miniML.parser.Integer
+import org.miniML.parser.{ExpressionParser, Integer}
 
 @RunWith(classOf[JUnitRunner])
 class TypeCheckerSuite extends FunSuite {
 
+
     test("integer") {
         val t = TypeChecker(Integer(0))
         assert( t == Right(TypeChecker.Nat()))
+    }
+
+    val ep = new ExpressionParser()
+
+    def check(s:String, et:EType) : Unit = {
+        val t = TypeChecker(ep.parse(s).get)
+        assert(t.contains(et))
+    }
+
+    test("sum") {
+        check("2+2", TypeChecker.Nat())
     }
 }
